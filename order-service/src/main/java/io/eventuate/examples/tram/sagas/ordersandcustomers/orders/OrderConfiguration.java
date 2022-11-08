701e08b5111c9776c5ec8ca3bb98d5189dc7bd26
ts-maksym.sharov <ts-maksym.sharov@rakuten.com>
2022-11-08 22:40:19 +0900

    fixing date time

--- order-service/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/orders/OrderConfiguration.java
+++ order-service/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/orders/OrderConfiguration.java
@@ -1,18 +1,21 @@
 package io.eventuate.examples.tram.sagas.ordersandcustomers.orders;
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.eventuate.common.json.mapper.JSonMapper;
+import io.eventuate.examples.tram.sagas.ordersandcustomers.orders.domain.OrderRepository;
 import io.eventuate.examples.tram.sagas.ordersandcustomers.orders.service.CreateOrderSaga;
 import io.eventuate.examples.tram.sagas.ordersandcustomers.orders.service.OrderSagaService;
-import io.eventuate.examples.tram.sagas.ordersandcustomers.orders.domain.OrderRepository;
 import io.eventuate.examples.tram.sagas.ordersandcustomers.orders.service.OrderService;
 import io.eventuate.examples.tram.sagas.ordersandcustomers.orders.service.UpdateOrderSaga;
 import io.eventuate.tram.sagas.orchestration.SagaInstanceFactory;
-import io.eventuate.tram.sagas.spring.orchestration.SagaOrchestratorConfiguration;
 import io.eventuate.tram.spring.optimisticlocking.OptimisticLockingDecoratorConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 @EnableJpaRepositories
@@ -38,5 +41,13 @@ public class OrderConfiguration {
   @Bean
   public UpdateOrderSaga updateOrderSaga() {
     return new UpdateOrderSaga();
+  }
+
+  /**
+   * To fix bug with Saga isn't able to serialize/deserialize java.time
+   */
+  @PostConstruct
+  public void init() {
+    JSonMapper.objectMapper.registerModule(new JavaTimeModule());
   }
 }
